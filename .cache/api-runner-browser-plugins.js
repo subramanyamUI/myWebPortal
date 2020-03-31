7daade3cad0081ac1d2fe67baf177d551e125e82
subramanyam reddy <subramanyam1105@gmail.com>
2020-03-31 14:57:11 +0530

    gatsby tailwind config

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
+      options: {"plugins":[],"name":"gatsby-tailwind-boilerplate","short_name":"starter","start_url":"/","background_color":"#ffffff","theme_color":"#007FFF","display":"browser","icon":"./src/assets/favicon.svg"},
+    },{
+      plugin: require('../gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,30 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Tailwind Boilerplate`,
+    description: `Boilerplate for projects with TailwindCSS and GatsbyJS`,
+    author: `@wirtzdan`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `gatsby-tailwind-boilerplate`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
+        background_color: `#ffffff`,
+        theme_color: `#007FFF`,
+        display: `browser`,
+        icon: `./src/assets/favicon.svg`
+      }
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
-}
+    `gatsby-plugin-postcss`,
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        tailwind: true,
+        purgeOnly: [`src/css/style.css`]
+      }
+    }
+  ]
+};
